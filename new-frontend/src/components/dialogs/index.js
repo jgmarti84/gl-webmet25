f94e2c916e8cafcf7b3352a68519501733fcbe8e
jgmarti841 <javier@fourthtech.ai>
2026-03-10 19:35:52 -0300

    JM: staged files for initial gitlab commit

--- new-frontend/src/components/dialogs/index.js
+++
@@ -1,6 +0,0 @@
-// Componentes de diálogos y modales
-export { default as ProductSelectorDialog } from "./ProductSelectorDialog";
-export { default as PseudoRHIDialog } from "./PseudoRHIDialog";
-export { default as AreaStatsDialog } from "./AreaStatsDialog";
-export { default as ElevationProfileDialog } from "./ElevationProfileDialog";
-export { default as LayerManagerDialog } from "./LayerManagerDialog";
--- new-frontend/src/utils/session.js
+++
@@ -1,4 +0,0 @@
-// Generar ID de sesión único para esta instancia del navegador
-export function generateSessionId() {
-  return `session-${Date.now()}-${Math.random().toString(36).substring(2, 15)}`;
-}
